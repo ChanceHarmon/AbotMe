e0e81cbcabbef7f958d362f2d94be864a9970884
Chance Harmon <henrikillsit@gmail.com>
2019-02-07 18:16:34 -0800

    Finished with Lab 4 finally

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var name = prompt('If you are going to learn about me, I would like to know your name, please tell me!');
 console.log(name);
 
+function questionOne() {
 var birth = prompt("Was I born in Washington State?");
 console.log(birth);
   
@@ -11,7 +12,9 @@ if (birth.toLowerCase() === 'yes'){
     else {
         alert ('Correct! I was born and raised in Montana, the Treasure State!');
     }
+  }
 
+function questionTwo() {
 var school = prompt('Did I finish High School?');
     console.log(school);
     if (school.toLowerCase() === 'yes'){
@@ -20,6 +23,9 @@ var school = prompt('Did I finish High School?');
     else {
         alert ('oh yee of little faith...');
     }
+  }
+
+function questionThree() {
 var bones = prompt(name + ', Do you think I have ever broken a bone?');
     console.log(bones);
     if (bones.toLowerCase() === 'yes'){
@@ -28,6 +34,9 @@ var bones = prompt(name + ', Do you think I have ever broken a bone?');
     else {
         alert ('I would not say I am accident prone, but i have been wreckless on a few of life\'s occassions...');
     }
+  }
+
+function questionFour() {
 var years = prompt('How many years have I been at my current employer? Please spell out your answer.');
     console.log(years);
     if (years.toLowerCase() === 'ten'){
@@ -36,6 +45,9 @@ var years = prompt('How many years have I been at my current employer? Please sp
     else {
         alert ('If you answered less than Ten, now you know I am a loyal employee!');
     }
+  }
+
+function questionFive() {
 var genius = prompt('Do I know what I am doing?');
     console.log(genius);
     if (genius.toLowerCase() === 'no'){
@@ -44,8 +56,9 @@ var genius = prompt('Do I know what I am doing?');
     else {
         alert ('I could sell you a pen for \$100 all day!');
     }
+  } 
 
-
+function questionSix() {
 var count = prompt('How many tattoos do I have?');
 console.log(prompt);
 for (var i = 0; i < 3; i++){
@@ -58,7 +71,9 @@ for (var i = 0; i < 3; i++){
   }
   count = prompt('Try again');
 }
+}
 
+function questionSeven() {
 var counter = 0;
 
 for (var i = 0; i < 7; i++) {
@@ -73,6 +88,17 @@ console.log(instrument);
     }
   } 
    alert('You got ' + counter + ' Points out of 7');
+}
+
+questionOne();
+questionTwo();
+questionThree();
+questionFour();
+questionFive();
+questionSix();
+questionSeven();
+
+
   
     
   
